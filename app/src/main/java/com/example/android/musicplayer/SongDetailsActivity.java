cf51c3d2e7b868abc8199b0dd6cb7767c3c95c39
alez <alez1986@gmail.com>
2018-04-15 10:53:58 +0300

    Added images with different resolutions

--- app/src/main/java/com/example/android/musicplayer/SongDetailsActivity.java
+++ app/src/main/java/com/example/android/musicplayer/SongDetailsActivity.java
@@ -1,19 +1,73 @@
 package com.example.android.musicplayer;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import javax.xml.datatype.Duration;
 
 
 public class SongDetailsActivity extends AppCompatActivity {
+
+    private MediaPlayer mMediaPlayer;
+
+    private AudioManager mAudioManager;
+
+    private float mVolume = 0;
+    private float mLowVolume = 0.3f;
+
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
+            releaseMediaPlayer();
+        }
+    };
+
+    /**
+     * This listener gets triggered whenever the audio focus changes
+     * (i.e., we gain or lose audio focus because of another app or device).
+     */
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT && mMediaPlayer != null) {
+                mMediaPlayer.pause();
+            }
+            else if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK && mMediaPlayer != null) {
+                float mVolume= mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+                mMediaPlayer.setVolume(mLowVolume, mLowVolume);
+            }
+            else if (focusChange == AudioManager.AUDIOFOCUS_GAIN && mMediaPlayer != null) {
+                mMediaPlayer.setVolume(mVolume, mVolume);
+                mMediaPlayer.start();
+            }
+            else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                releaseMediaPlayer();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_details);
+
+        // Create and setup the {@link AudioManager} to request audio focus
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_song);
         setSupportActionBar(toolbar);
@@ -32,11 +86,64 @@ public class SongDetailsActivity extends AppCompatActivity {
         artistText.setText(getIntent().getStringExtra("artist"));
         TextView songText = (TextView) findViewById(R.id.title);
         songText.setText(getIntent().getStringExtra("title"));
+
+        mMediaPlayer = MediaPlayer.create(this, R.raw.sample);
+
+        Button playButton = (Button) findViewById(R.id.details_play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mMediaPlayer.start();
+                    mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mediaPlayer) {
+                            releaseMediaPlayer();
+                            Toast.makeText(SongDetailsActivity.this, "I'm done!", Toast.LENGTH_SHORT);
+                        }
+                    });
+                }
+            }
+        });
+
+        Button pauseButton = (Button) findViewById(R.id.details_pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMediaPlayer.pause();
+            }
+        });
+
+        Button stopButton = (Button) findViewById(R.id.details_stop_button);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+            }
+        });
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
     }
 }
